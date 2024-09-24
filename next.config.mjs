c5c0b3b5b3d2e553a64a339989159ede709bed25
ayeshm3 <129223497+ayeshm3@users.noreply.github.com>
2024-09-23 19:02:16 -0700

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // experimental: {
+    //     appDir: true, // Enable app directory feature
+    // },
+    // Additional configurations can be added here
+};
+
+export default nextConfig;
